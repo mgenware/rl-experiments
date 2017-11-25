4d55ba91b7a6035e63dceca0c73b5dafdefe674d
Mgen <m@mgenware.com>
2017-11-25 16:36:53 +0800

    Display actions on result table

--- value-iteration/value-iteration.py
+++ value-iteration/value-iteration.py
@@ -2,14 +2,20 @@ import numpy as np
 import operator
 import sys
 
-WIDTH = 4
-HEIGHT = 3
+# grid size
+WIDTH       = 4
+HEIGHT      = 3
 # probabilities of each actions
-PROB_ACT_1 = .8
-PROB_ACT_2 = .1
-PROB_ACT_3 = .1
-# number of actions 
-ACT_COUNT = 3
+PROB_ACT_1  = .8
+PROB_ACT_2  = .1
+PROB_ACT_3  = .1
+# number of possible actions
+ACT_MAX     = 3
+# action names
+ACT_LEFT    = '←'
+ACT_RIGHT   = '→'
+ACT_UP      = '↑'
+ACT_DOWN    = '↓'
 
 # discount factor
 DISC_FACTOR = .9
@@ -19,11 +25,11 @@ def new_states():
 
 def next_policies(state, ignored_states):
     def add_action(action, prob, list):
-        if action == 'l':
+        if action == ACT_LEFT:
             offset = (0, -1)
-        elif action == 'r':
+        elif action == ACT_RIGHT:
             offset = (0, 1)
-        elif action == 'u':
+        elif action == ACT_UP:
             offset = (-1, 0)
         else:
             offset = (1, 0)
@@ -40,20 +46,23 @@ def next_policies(state, ignored_states):
 
     result = []
     # 4 possible actions: l(left) r(right) u(up) d(down)
-    for action in 'lrud':
+    for action in [ACT_LEFT, ACT_RIGHT, ACT_UP, ACT_DOWN]:
         policy = []
-        if action == 'l' or action == 'r':
+        if action == ACT_LEFT or action == ACT_RIGHT:
             add_action(action, PROB_ACT_1, policy)
-            add_action('u', PROB_ACT_2, policy)
-            add_action('d', PROB_ACT_3, policy)
+            add_action(ACT_UP, PROB_ACT_2, policy)
+            add_action(ACT_DOWN, PROB_ACT_3, policy)
         else:
             add_action(action, PROB_ACT_1, policy)
-            add_action('l', PROB_ACT_2, policy)
-            add_action('r', PROB_ACT_3, policy)
+            add_action(ACT_LEFT, PROB_ACT_2, policy)
+            add_action(ACT_RIGHT, PROB_ACT_3, policy)
         result.append(policy)
     return result
 
 def expected_value(current_state, policy, values, blocked_states):
+    if not len(policy):
+        raise Exception('No action found in this policy')
+
     def get_value(state, values, fallback_value, blocked_states):
         (row, col) = state
         if col < 0 or row < 0 or col >= WIDTH or row >= HEIGHT or state in blocked_states:
@@ -66,7 +75,7 @@ def expected_value(current_state, policy, values, blocked_states):
         ev += get_value(action['state'], values, current_value, blocked_states) * action['prob']
 
     ev *= DISC_FACTOR
-    return ev
+    return { 'action': policy[0]['action'], 'ev': ev }
 
 def main():
     args = sys.argv[1:]
@@ -79,6 +88,9 @@ def main():
     # set goal and dead end
     values[0, 3] = 1
     values[1, 3] = -1
+
+    # selected actions
+    actions = np.zeros((HEIGHT, WIDTH), dtype=str)
 
     # states you can't access
     blocked_states = set([(1, 1)])
@@ -94,11 +106,18 @@ def main():
         for state in enumerable_values:
             policies = next_policies(state, blocked_states)
             # max expected value
-            new_values[state] = max([expected_value(state, policy, values, blocked_states) for policy in policies])
+            max_policy = max([expected_value(state, policy, values, blocked_states) for policy in policies], key=lambda x:x['ev'])
+            new_values[state] = max_policy['ev']
+            actions[state] = max_policy['action']
         values = new_values
     
     result = np.round(np.matrix(values), 3)
-    print(result)
+
+    for x in range(0, HEIGHT):
+        for y in range(0, WIDTH):
+            action = actions[x, y]
+            print((str(round(values[x, y], 3)) + action).rjust(7), end=' ')
+        print()
 
 if __name__ == "__main__":
     main()
