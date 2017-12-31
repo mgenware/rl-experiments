9348603165e863a9d17bbd60cf5c931bd248ba74
Mgen <m@mgenware.com>
2017-12-31 12:21:32 +0800

    Add pmf-and-cdf

--- histogram-and-cdf/main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-mu, sigma = 175, 10
-heights = sigma * np.random.randn(100) + mu
-print(heights)
-
-hist = plt.hist(heights, bins=20, edgecolor='black', linewidth=1)
-plt.show()
---
+++ pmf-and-cdf/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+colors = ['gold', 'yellowgreen', 'c', 'royalblue', 'pink']
+
+# randomly generate the number of occurrences of each color
+occurrences = np.random.randint(10, size=len(colors))
+# pmf of the distribution
+sum = np.sum(occurrences)
+pmf = occurrences / sum
+
+print(pmf)
+
+# plotting
+bars = np.arange(len(colors))
+fig, ax = plt.subplots(2)
+
+ax[0].bar(bars, pmf, color=colors)
+
+plt.show()
