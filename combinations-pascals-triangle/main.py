09703f0e789e0f7c1f1e2b8bcfaebf2d03fe5395
Mgen <m@mgenware.com>
2018-02-06 01:31:58 +0800

    Update arguments processing

--- combinations-pascals-triangle/main.py
+++ combinations-pascals-triangle/main.py
@@ -7,13 +7,16 @@ def nCr(n,r):
   return f(n) / f(r) / f(n-r)
 
 args = sys.argv[1:]
-if not len(args):
+if len(args) != 1:
+  print("Invalid arguments. Example: main.py 12")
   sys.exit(1)
 n = int(args[0])
 if n <= 0:
+  print("n cannot be less than or equal to 0")
   sys.exit(2)
 
-print('n: ', n)
+
+print('n:', n)
 
 for x in range(0, n):
   res = int(nCr(n - 1, x))
